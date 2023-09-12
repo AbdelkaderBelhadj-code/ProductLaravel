cd748f7f09561490746d51453dd7e1b63805dadf
AbdelkaderBelhadj-code <belhajabdelkader3@gmail.com>
2023-09-12 15:39:43 +0100

    adding bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,49 @@
-<html lang="en">
+<!DOCTYPE html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <title>Inscription</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <div style="border: 3px solid #000000">
-    <h2>Register</h2>
-    <form action="/register" method="POST">
-        @csrf
-        <input name="name" type="text" placeholder="name" value="{{ old('name') }}">
-        @error('name')
-            <p class="text-danger">{{ $message }}</p>
-        @enderror
-        <input name="email" type="text" placeholder="email" value="{{ old('email') }}">
-        @error('email')
-        <p class="text-danger">{{ $message }}</p>
-        @enderror   
-        <input name="password"type="password" placeholder="password"  value="{{ old('password') }}">
-        @error('password')
-        <p class="text-danger">{{ $message }}</p>
-        @enderror  
-        <button>Register</button>
-
-
-    </form>
-    
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="border p-4">
+                    <h2>Inscription</h2>
+                    <form action="/register" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label for="name">Nom</label>
+                            <input name="name" type="text" class="form-control" id="name" placeholder="Nom" value="{{ old('name') }}">
+                            @error('name')
+                                <p class="text-danger">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Adresse e-mail</label>
+                            <input name="email" type="email" class="form-control" id="email" placeholder="Adresse e-mail" value="{{ old('email') }}">
+                            @error('email')
+                                <p class="text-danger">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Mot de passe</label>
+                            <input name="password" type="password" class="form-control" id="password" placeholder="Mot de passe" value="{{ old('password') }}">
+                            @error('password')
+                                <p class="text-danger">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">Inscription</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
